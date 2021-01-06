0a71e8e2f196d6c25fa125bc98b7fb6be5e710cc
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-06 11:03:57 +0100

    read dup

---
+++ ch1/ex1.4.go
@@ -0,0 +1,43 @@
+// prints the count of lines that occur more than once in a file
+// in the input. Reads from a list of file names or stdin if none are provided
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+			}
+
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, count := range counts {
+		if count > 1 {
+			fmt.Printf("%d\t%s\n", count, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
