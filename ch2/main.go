ae8bac93c069c49407b2014652aa0d0349f209a2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-06 18:43:15 +0100

    main uses tempconv package

--- ch2/main.go
+++ ch2/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"go_blunders/ch2/tempconv"
+)
+
+var temperature = flag.Float64("temperature", 0.0, "The temperature to be converted")
+
 func main() {
-	
+	flag.Parse()
+
+	fmt.Printf(
+		"%f = %f, %f = %f\n",
+		tempconv.Celsius(*temperature),
+		tempconv.CToF(tempconv.Celsius(*temperature)),
+		tempconv.Fahrenheit(*temperature),
+		tempconv.FToC(tempconv.Fahrenheit(*temperature)),
+	)
 }
