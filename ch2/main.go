28fb1b88f70201e0d97f7de0c684808dd6db1838
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-06 18:45:19 +0100

    changed flag

--- ch2/main.go
+++ ch2/main.go
@@ -6,16 +6,16 @@ import (
 	"go_blunders/ch2/tempconv"
 )
 
-var temperature = flag.Float64("temperature", 0.0, "The temperature to be converted")
+var temp = flag.Float64("temp", 0.0, "The temperature to be converted")
 
 func main() {
 	flag.Parse()
 
 	fmt.Printf(
 		"%s = %s, %s = %s\n",
-		tempconv.Celsius(*temperature),
-		tempconv.CToF(tempconv.Celsius(*temperature)),
-		tempconv.Fahrenheit(*temperature),
-		tempconv.FToC(tempconv.Fahrenheit(*temperature)),
+		tempconv.Celsius(*temp),
+		tempconv.CToF(tempconv.Celsius(*temp)),
+		tempconv.Fahrenheit(*temp),
+		tempconv.FToC(tempconv.Fahrenheit(*temp)),
 	)
 }
