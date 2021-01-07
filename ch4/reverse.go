ab289b74d9267f6f07919b1630247fcc18d40630
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-07 14:54:18 +0100

    Reverse in place and with a copy

--- ch4/reverse.go
+++ ch4/reverse.go
@@ -5,13 +5,29 @@ import "fmt"
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
 
-	reverseInt(numbers)
+	reverseIntInPlace(numbers)
 
+	fmt.Println(numbers)
+	fmt.Println(reverseInt(numbers))
 	fmt.Println(numbers)
 }
 
-func reverseInt(original []int) {
+// reverseIntInPlace reverses a slice of int in place
+func reverseIntInPlace(original []int) {
 	for i, j := 0, len(original)-1; i < j; i, j = i+1, j-1 {
 		original[i], original[j] = original[j], original[i]
 	}
+}
+
+// reverseInt returns a reverse of the input slice
+func reverseInt(original []int) []int {
+	reverse := make([]int, len(original), cap(original))
+	reverseIndex := len(original) - 1
+
+	for _, v := range original {
+		reverse[reverseIndex] = v
+		reverseIndex--
+	}
+
+	return reverse
 }
