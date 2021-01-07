f46ccdcbe137f6b924503d83452ee3522d8c66f5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-07 15:03:38 +0100

    Completed append int

--- ch4/appendInt.go
+++ ch4/appendInt.go
@@ -6,6 +6,21 @@ func main() {
 
 func appendInt(original []int, newItem int) []int {
 	var result []int
+	newLength := len(original) + 1
 
+	if newLength <= cap(original) {
+		result = original[:newLength]
+	} else {
+		newCap := newLength
+
+		if newCap < 2*len(original) {
+			newCap = 2 * len(original)
+		}
+
+		result = make([]int, newLength, newCap)
+		copy(result, original)
+	}
+
+	result[newLength] = newItem
 	return result
 }
