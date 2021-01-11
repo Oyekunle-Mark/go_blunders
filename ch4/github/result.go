eb194e35f9e0bb3deb5c7e1216a337237f5c86e3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-11 19:26:06 +0100

    completed sing method

--- ch4/github/result.go
+++ ch4/github/result.go
@@ -7,14 +7,19 @@ type Singer interface {
 }
 
 type Artiste struct {
-	Name string
-	NIN string
-	ID int
+	Name  string
+	Genre string
+	ID    int
 }
 
 func (a *Artiste) SingHeartOut(lyrics []string) error {
 	if len(lyrics) <= 0 {
-		return fmt.Errorf("%s", )
+		return fmt.Errorf("error by singer %s - id %d", a.Name, a.ID)
 	}
+
+	for _, word := range lyrics {
+		fmt.Printf("%s", word)
+	}
+
 	return nil
 }
