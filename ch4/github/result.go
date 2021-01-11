7c13ff58c6f0dd758be34d52c6f781ec79c6cc8b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-11 19:23:19 +0100

    Added singer interface

--- ch4/github/result.go
+++ ch4/github/result.go
@@ -1,1 +1,20 @@
 package github
+
+import "fmt"
+
+type Singer interface {
+	SingHeartOut(lyrics []string) error
+}
+
+type Artiste struct {
+	Name string
+	NIN string
+	ID int
+}
+
+func (a *Artiste) SingHeartOut(lyrics []string) error {
+	if len(lyrics) <= 0 {
+		return fmt.Errorf("%s", )
+	}
+	return nil
+}
