95f7eeeaa48ab41d01782432461f61f0b2cee045
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-13 15:51:56 +0100

    Added a benchmark

--- ch11/palindrome/word_test.go
+++ ch11/palindrome/word_test.go
@@ -57,3 +57,9 @@ func TestIsPalindrome(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkIsPalindrome(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IsPalindrome("A man, a plan, a canal: Panama")
+	}
+}
