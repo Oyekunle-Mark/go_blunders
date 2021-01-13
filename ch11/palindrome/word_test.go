ed38e131844f6b24d6498c393aca762e50e7ade5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-13 14:45:36 +0100

    checks if tests to palindrome are correct

--- ch11/palindrome/word_test.go
+++ ch11/palindrome/word_test.go
@@ -1,1 +1,13 @@
 package palindrome
+
+import "testing"
+
+func TestPalindrome(t *testing.T) {
+	if !IsPalindrome("detartracted") {
+		t.Error(`IsPalindrome("detartrated") = false`)
+	}
+
+	if !IsPalindrome("kayak") {
+		t.Error(`IsPalindrome("kayak") = false`)
+	}
+}
