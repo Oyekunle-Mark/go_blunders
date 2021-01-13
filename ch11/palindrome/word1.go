0d034a9233b5841901e1739e1f822448c8b11248
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-13 14:42:26 +0100

    Documented function

--- ch11/palindrome/word1.go
+++ ch11/palindrome/word1.go
@@ -1,5 +1,8 @@
 package palindrome
 
+// IsPalindrome checks if a string is palindrome
+// returns true if it is and false otherwise
+// (unoptimised)
 func IsPalindrome(s string) bool {
 	for i := range s {
 		if s[i] != s[len(s)-1-i] {
