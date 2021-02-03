91fe7f75b56ad3b2b07f3695d9307700f404bc1e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-02-03 09:20:49 +0100

    First pass

--- leetcode_feb/trimBST.go
+++ leetcode_feb/trimBST.go
@@ -7,4 +7,18 @@ type TreeNode struct {
 }
 
 func trimBST(root *TreeNode, low int, high int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val < low {
+		return root.Right
+	}
+	if root.Val > high {
+		return root.Left
+	}
+
+	root.Left = trimBST(root.Left, low, high)
+	root.Right = trimBST(root.Right, low, high)
+
+	return root
 }
