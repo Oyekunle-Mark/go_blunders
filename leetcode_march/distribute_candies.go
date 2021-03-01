bcefe44c6822fa64aabb65b01b6a71f59a852160
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-01 10:28:09 +0100

    commented solution

--- leetcode_march/distribute_candies.go
+++ leetcode_march/distribute_candies.go
@@ -1,20 +1,23 @@
 package main
 
 func distributeCandies(candyType []int) int {
-	candySet := make(map[int]bool)
-	var distinctCandies []int
+	candySet := make(map[int]bool) // act as a set of distinct candies
+	var distinctCandies []int      // hold distinct candies
 	maxCandies := len(candyType) / 2
 
+	// for all candy type
 	for _, candy := range candyType {
+		// if current candy isn't in set
 		if _, ok := candySet[candy]; !ok {
-			distinctCandies = append(distinctCandies, candy)
-			candySet[candy] = true
+			distinctCandies = append(distinctCandies, candy) // add to distinct candies slice
+			candySet[candy] = true                           // add to set
 		}
 	}
 
+	// if distinct candies is less than the max, return len of distinc
 	if len(distinctCandies) < maxCandies {
 		return len(distinctCandies)
 	}
 
-	return maxCandies
+	return maxCandies // otherwise, she can eat the max
 }
