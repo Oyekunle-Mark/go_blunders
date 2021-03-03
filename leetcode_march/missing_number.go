93b8f6544b7ac1cc883b8dac039eeae02eb1ef17
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-03 10:14:21 +0100

    documented the efficient second pass

--- leetcode_march/missing_number.go
+++ leetcode_march/missing_number.go
@@ -1,16 +1,18 @@
 package main
 
 func missingNumber(nums []int) int {
-	var numsSum int
-	maxNum := len(nums)
+	var numsSum int     // hold the sum of integers in num
+	maxNum := len(nums) // maximum number
 
+	// find numsSum
 	for i := 0; i <= maxNum; i++ {
 		numsSum += i
 	}
 
+	// subtract every integer in nums from numsSum
 	for _, num := range nums {
 		numsSum -= num
 	}
 
-	return numsSum
+	return numsSum // remainder is the missing number
 }
