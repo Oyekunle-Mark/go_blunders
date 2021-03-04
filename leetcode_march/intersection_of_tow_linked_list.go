d1f50b065c7840c10c3ec8a7c383ab262c56d82f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-04 11:39:14 +0100

    second pass solution

--- leetcode_march/intersection_of_tow_linked_list.go
+++ leetcode_march/intersection_of_tow_linked_list.go
@@ -6,26 +6,46 @@ type ListNode struct {
 }
 
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
-	nodeStore := make(map[*ListNode]bool) // hold list node from lists
+	//nodeStore := make(map[*ListNode]bool) // hold list node from lists
+	//
+	//current := headA // head of list headA
+	//
+	//// for all node, store
+	//for current != nil {
+	//	nodeStore[current] = true
+	//	current = current.Next
+	//}
+	//
+	//current = headB // head of list headB
+	//
+	//// for all node in headB, the first node found in store is intersection point
+	//for current != nil {
+	//	if _, ok := nodeStore[current]; ok {
+	//		return current
+	//	}
+	//
+	//	current = current.Next
+	//}
+	//
+	//return nil // not found
 
-	current := headA // head of list headA
+	// second pass
+	currentA := headA
+	currentB := headB
 
-	// for all node, store
-	for current != nil {
-		nodeStore[current] = true
-		current = current.Next
-	}
-
-	current = headB // head of list headB
-
-	// for all node in headB, the first node found in store is intersection point
-	for current != nil {
-		if _, ok := nodeStore[current]; ok {
-			return current
+	for currentA != currentB {
+		if currentA == nil {
+			currentA = headB
+		} else {
+			currentA = currentA.Next
 		}
 
-		current = current.Next
+		if currentB == nil {
+			currentB = headA
+		} else {
+			currentB = currentB.Next
+		}
 	}
 
-	return nil // not found
+	return currentA
 }
