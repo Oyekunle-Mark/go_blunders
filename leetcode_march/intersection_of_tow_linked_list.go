dada4cfae84e924bb4e7bafc9d271776517948a1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-04 11:23:32 +0100

    first pass

--- leetcode_march/intersection_of_tow_linked_list.go
+++ leetcode_march/intersection_of_tow_linked_list.go
@@ -6,6 +6,24 @@ type ListNode struct {
 }
 
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	nodeStore := make(map[*ListNode]bool)
 
+	current := headA
+
+	for current != nil {
+		nodeStore[current] = true
+		current = current.Next
+	}
+
+	current = headB
+
+	for current != nil {
+		if _, ok := nodeStore[current]; ok {
+			return current
+		}
+
+		current = current.Next
+	}
+
+	return nil
 }
-
