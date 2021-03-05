dcd69ca8943434504d53c92416ed032090dabeb7
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-05 18:08:55 +0100

    find the avearge of node values

--- leetcode_march/average_of_levels_in_bst.go
+++ leetcode_march/average_of_levels_in_bst.go
@@ -20,6 +20,16 @@ func levelOrderNodes(node *TreeNode, current int, target int, nodeVals []int) {
 		return
 	}
 
-	levelOrderNodes(node.Left, current + 1, target, nodeVals)
-	levelOrderNodes(node.Right, current + 1, target, nodeVals)
+	levelOrderNodes(node.Left, current+1, target, nodeVals)
+	levelOrderNodes(node.Right, current+1, target, nodeVals)
+}
+
+func findAverage(nodeVals []int) float64 {
+	var sum float64 = 0
+
+	for _, val := range nodeVals {
+		sum += float64(val)
+	}
+
+	return sum / float64(len(nodeVals))
 }
