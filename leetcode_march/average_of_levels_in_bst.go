64e516f19d95f40008945fc334126b79b318aef4
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-05 18:02:44 +0100

    Print the in order value of a tree nodes

--- leetcode_march/average_of_levels_in_bst.go
+++ leetcode_march/average_of_levels_in_bst.go
@@ -9,3 +9,17 @@ type TreeNode struct {
 func averageOfLevels(root *TreeNode) []float64 {
 
 }
+
+func inOrderNodes(node *TreeNode, current int, target int, nodeVals []int) {
+	if node == nil {
+		return
+	}
+
+	if current == target {
+		nodeVals = append(nodeVals, node.Val)
+		return
+	}
+
+	inOrderNodes(node.Left, current + 1, target, nodeVals)
+	inOrderNodes(node.Right, current + 1, target, nodeVals)
+}
