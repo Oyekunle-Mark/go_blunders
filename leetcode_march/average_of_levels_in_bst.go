13254bd3764c4d7df4340bce767f4549f5891dde
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-05 18:43:54 +0100

    Documented the algorithm

--- leetcode_march/average_of_levels_in_bst.go
+++ leetcode_march/average_of_levels_in_bst.go
@@ -7,18 +7,20 @@ type TreeNode struct {
 }
 
 func averageOfLevels(root *TreeNode) []float64 {
-	treeHeight := findTreeHeight(root, 0)
-	var averageNodePerLevel []float64
+	treeHeight := findTreeHeight(root, 0) // find tree height
+	var averageNodePerLevel []float64     // to hold the averages
 
+	// loop for every level int the tree
 	for i := 1; i <= treeHeight; i++ {
-		var nodesAtCurrentLevel []int
-		levelOrderNodes(root, 1, i, &nodesAtCurrentLevel)
-		averageNodePerLevel = append(averageNodePerLevel, findAverage(nodesAtCurrentLevel))
+		var nodesAtCurrentLevel []int                                                       // will hold current node val level i
+		levelOrderNodes(root, 1, i, &nodesAtCurrentLevel)                                   // write node val to nodesAtCurrentLevel
+		averageNodePerLevel = append(averageNodePerLevel, findAverage(nodesAtCurrentLevel)) // append the average to averageNodePerLevel
 	}
 
-	return averageNodePerLevel
+	return averageNodePerLevel // return result
 }
 
+// levelOrderNodes appends the Val of all node at level target to nodeVals as side effect
 func levelOrderNodes(node *TreeNode, current int, target int, nodeVals *[]int) {
 	if node == nil {
 		return
@@ -33,6 +35,7 @@ func levelOrderNodes(node *TreeNode, current int, target int, nodeVals *[]int) {
 	levelOrderNodes(node.Right, current+1, target, nodeVals)
 }
 
+// findAverage finds the average on the input slice of int and returns the result as a float64
 func findAverage(nodeVals []int) float64 {
 	var sum float64 = 0
 
@@ -43,6 +46,7 @@ func findAverage(nodeVals []int) float64 {
 	return sum / float64(len(nodeVals))
 }
 
+// findTreeHeight finds the height of a bst.
 func findTreeHeight(root *TreeNode, level int) int {
 	if root == nil {
 		return level
