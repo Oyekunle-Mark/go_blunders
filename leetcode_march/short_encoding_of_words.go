0debaa040eedc4a1544549491d939e73f64bbeda
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-07 08:37:44 +0100

    implement the sort.Interface interface on a user defined type of slice of string

--- leetcode_march/short_encoding_of_words.go
+++ leetcode_march/short_encoding_of_words.go
@@ -2,6 +2,20 @@ package main
 
 import "strings"
 
+type ByLength []string
+
+func (s ByLength) Len() int {
+	return len(s)
+}
+
+func (s ByLength) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s ByLength) Less(i, j int) bool {
+	return len(s[i]) < len(s[j])
+}
+
 func minimumLengthEncoding(words []string) int {
 	encoding := ""
 
