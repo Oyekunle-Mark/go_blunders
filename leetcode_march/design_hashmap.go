1fd23fe2659573bbfcd0b7d3433dbbf24603dac0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-08 09:28:44 +0100

    Documented solution

--- leetcode_march/design_hashmap.go
+++ leetcode_march/design_hashmap.go
@@ -8,11 +8,13 @@ import (
 
 const BucketSize = 1000000
 
-type MapValue struct {
+// MapPair represents the hash map key, value pair
+type MapPair struct {
 	key   int
 	value int
 }
 
+// MyHashMap is the hash map type
 type MyHashMap struct {
 	buckets [BucketSize]*list.List
 }
@@ -31,7 +33,7 @@ func (m *MyHashMap) Hash(key int) uint {
 
 	h := djb2Hash(&buf)
 
-	return h % uint(BucketSize)
+	return h % uint(BucketSize) // return hash modulo bucket size to limit to bucket size
 }
 
 /** value will always be non-negative. */
@@ -39,18 +41,22 @@ func (m *MyHashMap) Put(key int, value int) {
 	mapIndex := m.Hash(key)
 	bucket := m.buckets[mapIndex]
 
+	// if current bucket hasn't been initialized with a dll
 	if bucket == nil {
-		m.buckets[mapIndex] = list.New()
-		m.buckets[mapIndex].PushBack(MapValue{key, value})
+		m.buckets[mapIndex] = list.New()                  // initialize
+		m.buckets[mapIndex].PushBack(MapPair{key, value}) // insert key, value pair
 	} else {
+		// search all dll elements for the key
 		for e := bucket.Front(); e != nil; e = e.Next() {
-			if e.Value.(MapValue).key == key {
-				e.Value = MapValue{key, value}
+			// if current element has the key
+			if e.Value.(MapPair).key == key {
+				e.Value = MapPair{key, value} // update and return with new pair
 				return
 			}
 		}
 
-		m.buckets[mapIndex].PushBack(MapValue{key, value})
+		// no element found in dll with the key
+		m.buckets[mapIndex].PushBack(MapPair{key, value}) // insert and return
 		return
 	}
 }
@@ -60,17 +66,20 @@ func (m *MyHashMap) Get(key int) int {
 	mapIndex := m.Hash(key)
 	bucket := m.buckets[mapIndex]
 
+	// return early if bucket is uninitialized
 	if bucket == nil {
 		return -1
 	}
 
+	// loop through dll elements for key
 	for e := bucket.Front(); e != nil; e = e.Next() {
-		if e.Value.(MapValue).key == key {
-			return e.Value.(MapValue).value
+		// if found, return it's value
+		if e.Value.(MapPair).key == key {
+			return e.Value.(MapPair).value
 		}
 	}
 
-	return -1
+	return -1 // not found in dll at the bucket
 }
 
 /** Removes the mapping of the specified value key if this map contains a mapping for the key */
@@ -78,18 +87,22 @@ func (m *MyHashMap) Remove(key int) {
 	mapIndex := m.Hash(key)
 	bucket := m.buckets[mapIndex]
 
+	// return early if bucket is uninitialized
 	if bucket == nil {
 		return
 	}
 
+	// loop through dll elements for key
 	for e := bucket.Front(); e != nil; e = e.Next() {
-		if e.Value.(MapValue).key == key {
+		// call remove on dll with element if it has the key
+		if e.Value.(MapPair).key == key {
 			bucket.Remove(e)
 		}
 	}
 
 }
 
+// djb2Hash creates the hash
 func djb2Hash(buf *bytes.Buffer) uint {
 	var h uint = 5381
 
