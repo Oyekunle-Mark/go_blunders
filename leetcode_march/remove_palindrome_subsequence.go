d94ccd617c8d7df3fca1a72077f8d5e310f1dbd5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-08 19:00:01 +0100

    first pass

--- leetcode_march/remove_palindrome_subsequence.go
+++ leetcode_march/remove_palindrome_subsequence.go
@@ -1,7 +1,15 @@
 package main
 
 func removePalindromeSub(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
 
+	if isPalindrome(s) {
+		return 1
+	} else {
+		return 2
+	}
 }
 
 func isPalindrome(s string) bool {
