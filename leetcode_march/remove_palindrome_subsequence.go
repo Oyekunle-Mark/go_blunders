6f8e0e3c5aecf197397a2dee0e09eff61ad9244f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-08 19:01:37 +0100

    first pass which uses the trick that there are only two types of characters in the input string

--- leetcode_march/remove_palindrome_subsequence.go
+++ leetcode_march/remove_palindrome_subsequence.go
@@ -1,17 +1,20 @@
 package main
 
 func removePalindromeSub(s string) int {
+	// return 0 for empty string
 	if len(s) == 0 {
 		return 0
 	}
 
+	// if string is palindrome, return 1
 	if isPalindrome(s) {
 		return 1
-	} else {
+	} else { // otherwise, two
 		return 2
 	}
 }
 
+// isPalindrome returns true if s is a palindrome and false otherwise.
 func isPalindrome(s string) bool {
 	for i, c := range s {
 		if uint8(c) != s[len(s)-i-1] {
