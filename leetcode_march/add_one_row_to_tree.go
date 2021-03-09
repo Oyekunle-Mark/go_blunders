1f557f0d550e4c09fbe2e642efc04fc59da80c93
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-09 10:12:50 +0100

    TreeNode type redeclaration for the main package

--- leetcode_march/add_one_row_to_tree.go
+++ leetcode_march/add_one_row_to_tree.go
@@ -1,11 +1,13 @@
 package main
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
 func addOneRow(root *TreeNode, v int, d int) *TreeNode {
-
+	return root
 }
