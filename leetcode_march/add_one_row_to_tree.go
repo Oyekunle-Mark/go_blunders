03ff07c3103b060f61b0e2baeb5c0d95ef66bc07
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-09 10:39:33 +0100

    first pass

--- leetcode_march/add_one_row_to_tree.go
+++ leetcode_march/add_one_row_to_tree.go
@@ -18,26 +18,30 @@ func addOneRow(root *TreeNode, v int, d int) *TreeNode {
 		}
 	}
 
+	addNodeAtLevel(root, v, d, 1)
+
 	return root
 }
 
 func addNodeAtLevel(root *TreeNode, v, d, currentLevel int) {
+	if root == nil {
+		return
+	}
+
 	if currentLevel == d-1 {
-		if root.Left != nil {
-			root.Left = &TreeNode{
-				Val:   v,
-				Left:  root.Left,
-				Right: nil,
-			}
+		root.Left = &TreeNode{
+			Val:   v,
+			Left:  root.Left,
+			Right: nil,
 		}
 
-		if root.Right != nil {
-			root.Right = &TreeNode{
-				Val:   v,
-				Left:  nil,
-				Right: root.Right,
-			}
+		root.Right = &TreeNode{
+			Val:   v,
+			Left:  nil,
+			Right: root.Right,
 		}
+
+		return
 	}
 
 	addNodeAtLevel(root.Left, v, d, currentLevel+1)
