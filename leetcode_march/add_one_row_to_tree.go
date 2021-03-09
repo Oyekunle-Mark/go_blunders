782ebec1517078868b80232cfd8f5c2a8837aaa5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-09 10:25:08 +0100

    addNode at level adds node of value v at the desired level

--- leetcode_march/add_one_row_to_tree.go
+++ leetcode_march/add_one_row_to_tree.go
@@ -9,5 +9,37 @@ package main
  * }
  */
 func addOneRow(root *TreeNode, v int, d int) *TreeNode {
+	// when there is no depth d - 1
+	if d == 1 {
+		return &TreeNode{
+			Val:   v,
+			Left:  root,
+			Right: nil,
+		}
+	}
+
 	return root
+}
+
+func addNodeAtLevel(root *TreeNode, v, d, currentLevel int) {
+	if currentLevel == d-1 {
+		if root.Left != nil {
+			root.Left = &TreeNode{
+				Val:   v,
+				Left:  root.Left,
+				Right: nil,
+			}
+		}
+
+		if root.Right != nil {
+			root.Right = &TreeNode{
+				Val:   v,
+				Left:  nil,
+				Right: root.Right,
+			}
+		}
+	}
+
+	addNodeAtLevel(root.Left, v, d, currentLevel+1)
+	addNodeAtLevel(root.Right, v, d, currentLevel+1)
 }
